56fed91440722aa42df9e74ea6888f46ffdb8f44
Yoon Do Yeong <ehdud05264@naver.com>
2025-05-15 10:53:50 +0900

    사과게임 프로젝트 시작

---
+++ dozeo_appleGame/src/main/java/com/project/dozeo_appleGame/DozeoAppleGameApplication.java
@@ -0,0 +1,13 @@
+package com.project.dozeo_appleGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DozeoAppleGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DozeoAppleGameApplication.class, args);
+	}
+
+}
---
+++ dozeo_appleGame/src/test/java/com/project/dozeo_appleGame/DozeoAppleGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dozeo_appleGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DozeoAppleGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
