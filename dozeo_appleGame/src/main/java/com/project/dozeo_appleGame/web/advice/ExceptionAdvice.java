9022f6a1c9a0508d6fb4f3e1c3f56faffc598617
Yoon Do Yeong <ehdud05264@naver.com>
2025-05-17 19:53:09 +0900

    :  Swagger 문제 - ControllerAdviceBean 오류로 @~advoce~ , @~controller~ 를 사용한 클래스에서 문제가 있는 걸로 판단 - 해당 오류 문구를 검색해 찾을 결과 @RestControllerAdvice와 Swagger가 충돌이 발생하는 것으로 보임 - Spring boot 3.4.x 버전부터 둘이 충돌이 생긴 것으로 생각함. 이유는 직전에 만든 게임 전적 검색 프로젝트는 Spring boot 3.2.x를 사용했기 때문 - 임시적으로 @Hidden을 사용해 문제 해결

--- dozeo_appleGame/src/main/java/com/project/dozeo_appleGame/web/advice/ExceptionAdvice.java
+++ dozeo_appleGame/src/main/java/com/project/dozeo_appleGame/web/advice/ExceptionAdvice.java
@@ -1,7 +1,9 @@
 package com.project.dozeo_appleGame.web.advice;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Hidden
 @RestControllerAdvice
 public class ExceptionAdvice {
 
