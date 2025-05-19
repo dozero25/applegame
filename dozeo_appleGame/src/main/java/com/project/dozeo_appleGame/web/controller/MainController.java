b582462ab21af14b5483a18ff74a1ebf891388b9
Yoon Do Yeong <ehdud05264@naver.com>
2025-05-19 18:32:19 +0900

    - MainController에서 main, game 페이지 매핑 추가 - main.html, game.html 및 관련 CSS 구현

--- dozeo_appleGame/src/main/java/com/project/dozeo_appleGame/web/controller/MainController.java
+++ dozeo_appleGame/src/main/java/com/project/dozeo_appleGame/web/controller/MainController.java
@@ -1,7 +1,20 @@
 package com.project.dozeo_appleGame.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
+    @GetMapping("/main")
+    public String main(){
+        return "html/main/main.html";
+    }
+
+    @GetMapping("/game")
+    public String game(){
+        return "html/game/game.html";
+
+    }
+
 }
